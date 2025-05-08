3d24c5f96295cf27a91c00aec46856a416041fe4
pipisa <your@email.ex>
2025-05-08 08:52:16 +0300

    6 uzd

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class NoteController extends Controller
 {
-    //
+        public function index()
+    {
+        $notes = \App\Models\Note::all();
+        return view('notes.index', ['notes' => $notes]);
+    }
 }
